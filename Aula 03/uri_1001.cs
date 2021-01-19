60a10acf7b53b5ee7ef6ab173ebe2097df7e7595
Stefaneo Ribeiro <stefaneo.ribeiro@escolar.ifrn.edu.br>
2021-01-19 02:32:50 +0000

    Adicionado uri_1001

---
+++ Aula 03/uri_1001.cs
@@ -0,0 +1,10 @@
+using Systen;
+class MainClass{
+  public static void Main(string[] args){
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int X = a + b;
+
+    Console.WriteLine($"X = {x}");
+  }
+}
